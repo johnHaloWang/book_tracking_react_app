514d20a988405ff884c47774fe083c560de3aac5
John Wang <johnwang@Johns-MacBook-Pro.local>
2021-06-08 19:18:34 -0700

    Add Book, Bookshelf, SearchBook components

---
+++ src/Book.js
@@ -0,0 +1,29 @@
+import React, { Component }from 'react'
+// import Book from './Book'
+
+class Book extends React.Component{
+
+  render(){
+    console.log('Props', this.props)
+    return (
+      <div className="book">
+        <div className="book-top">
+           <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: this.props.book.bookUrl }}></div>
+          <div className="book-shelf-changer">
+            <select>
+              <option value="move" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{this.props.book.id}</div>
+        <div className="book-authors">{this.props.book.author}</div>
+      </div>
+    )
+  }
+}
+
+export default Book
---
+++ src/Bookshelf.js
@@ -0,0 +1,26 @@
+import React, { Component }from 'react'
+import Book from './Book'
+
+class Bookshelf extends React.Component{
+
+  render(){
+    // console.log('Props', this.props)
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Currently Reading</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.data.map((item)=>(
+              <li key={item.id}>
+                <Book book = {item}/>
+              </li>
+            ))}
+
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Bookshelf
